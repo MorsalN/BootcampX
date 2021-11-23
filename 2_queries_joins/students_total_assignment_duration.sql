f35b0c781fae51b75e95172f20038c1ec5b82b56
Morsal Niyaz <morsalniyaz@gmail.com>
2021-11-22 20:14:08 -0800

    BootcampX Queries II - JOIN

---
+++ 2_queries_joins/students_total_assignment_duration.sql
@@ -0,0 +1,5 @@
+-- Get the total amount of time that a student has spent on all assignments.
+SELECT SUM(assignment_submissions.duration) AS total_duration
+FROM assignment_submissions
+JOIN students ON student_id = students.id
+WHERE students.name = 'Ibrahim Schimmel';
